1e4a7a705b2c802b7f7b95dbf3a510ce6d919935
SalemZakkar <mr.salem1.sz@gmail.com>
2023-01-07 19:59:21 +0200

    first commit

---
+++ android/app/src/main/java/com/TheBlackPearl/salem_brick/MainActivity.java
@@ -0,0 +1,6 @@
+package com.TheBlackPearl.salem_brick;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/config/app_assets.dart
@@ -0,0 +1,10 @@
+String sound = "assets/sound";
+String img = "assets/img";
+
+class AppAssets {
+  static String pressed = "$sound/pressed.mp3";
+  static String win = "$sound/win.mp3";
+  static String lose = "$sound/lose.mp3";
+  static String hitBrick = "$sound/hitBrick.mp3";
+  static String hitBar = "$sound/hitBar.mp3";
+}
